8a7097204d378b55d965500e71659acd6b27fbb8
toan cv <toancvuit@gmail.com>
2022-03-13 07:40:05 +0700

    use hook in hp page

--- src/config.js
+++ src/config.js
@@ -7,10 +7,10 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ;
 //configure to server
 export const sagaMiddleware = createSagaMiddleware();
 
-//local store configure to debug
-export const store = createStore(reducer, composeEnhancer(applyMiddleware(sagaMiddleware)));
-// server configure
-// export const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+//local store and server configure to debug
 
-export const SERVER_URL = 'http://127.0.0.1:4001'
-// export const SERVER_URL = 'https://phuquy-server.herokuapp.com'
+// export const store = createStore(reducer, composeEnhancer(applyMiddleware(sagaMiddleware)));
+export const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+// export const SERVER_URL = 'http://127.0.0.1:4001'
+export const SERVER_URL = 'https://phuquy-server.herokuapp.com'
